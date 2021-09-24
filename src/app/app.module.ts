b73d7e6161a937642830d1f27b37883bd9c0f7cc
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-09-24 14:47:18 -0300

    Se agregan más componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { AsideComponent } from './compartida/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
